504fc3654d8cad77a92583afafb52af6b0a12e61
Krishan Wyse <kwysek@gmail.com>
2025-01-17 10:58:48 +0000

    add home page

--- lib/personal_website.ex
+++ lib/personal_website.ex
@@ -7,7 +7,20 @@ defmodule PersonalWebsite do
   def index(assigns) do
     ~H"""
     <.layout>
-      <h1 class="underline">Krishan Wyse</h1>
+      <main class="h-screen flex flex-col bg-slate-500">
+        <section class="grow flex">
+          <div class="container w-[400px] m-auto">
+            <h1 class="py-16 text-4xl text-neutral-100 font-serif italic">A whisper on the wind</h1>
+            <div class="py-4 text-2xl text-center text-neutral-300">{<<0x2666::utf8>>}</div>
+            <p class="py-6 text-xl text-center text-neutral-200">Check back soon</p>
+          </div>
+        </section>
+        <footer class="py-4">
+          <div class="text-center text-neutral-200 tracking-wider">
+            {<<0x00A9::utf8>>} {DateTime.utc_now() |> Map.fetch!(:year)} Krishan Wyse
+          </div>
+        </footer>
+      </main>
     </.layout>
     """
   end
@@ -15,6 +28,7 @@ defmodule PersonalWebsite do
   def layout(assigns) do
     ~H"""
     <html>
+      <meta charset="utf-8" />
       <link rel="stylesheet" href="/assets/app.css" />
       <script type="text/javascript" src="/assets/app.js" />
       <body>
